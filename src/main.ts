79ecef3c8f88b3a54e0584138fbd4b1fec3ca4cd
Dominik Kolarsky <kolarskydominik@gmail.com>
2024-02-04 13:08:29 +0100

    html, icons, css vars

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 import "./styles/index.css"
 
+
+function updateCurrentYear() {
+  const year = new Date().getFullYear();
+  const spanElement = document.querySelector('.footer_copyright')!;
+  spanElement.textContent = `© ${year} Dominik Kolarský`;
+}
+
+updateCurrentYear();
